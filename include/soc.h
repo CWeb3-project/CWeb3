7f13a2b3533743ae735c61062c8a6adb3bb6dd44
FireGen <71903267+FireGen-Loading@users.noreply.github.com>
2022-06-13 20:50:45 +0300

    stuff

--- include/soc.h
+++ include/soc.h
@@ -1,8 +1,18 @@
+#include "config.h"
 
 #ifndef __CWEB3_H_
 #define __CWEB3_H_
 #pragma once
 
-void newSocket();
+#ifdef __linux__ 
+
+typedef struct {
+    CWeb3Config config;
+    int scoketID;
+} CWeb3Socket;
+
+CWeb3Socket CWeb3_newSocket(CWeb3Config conf, int protocol);
+
+#endif /* __linux__ */
 
 #endif /* __CWEB3_H_ */
--- src/linSoc.c
+++ src/linSoc.c
@@ -1,4 +1,20 @@
+#include "soc.h"
+#include "config.h"
 
-int newSocket(){
-    
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+CWeb3Socket CWeb3_newSocket(CWeb3Config confing, int protocol){
+    CWeb3Socket socket;
+
+
 }
+
+int CWeb3_bind(int socket){
+    struct sockaddr_in server_addr;
+    
+
+}
+
