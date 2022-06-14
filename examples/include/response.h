c52b637a25052395cbf471002b471e6ee4c69242
FireGen <71903267+FireGen-Loading@users.noreply.github.com>
2022-06-14 12:30:36 +0300

    deleted accidentally added files

--- examples/include/response.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-typedef struct {
-    const char* content_type;
-} CWeb3Response;
-
-// TODO: make responses for HTML, JS, ...
-
--- examples/include/socketServer.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "soc.h"
-
-char* CWeb3Recv(CWeb3Socket clientSocket, size_t* pBufferSize);
